e3baea8c38dbb798500cbed81cfbb8018b310494
Rudra775 <sharmarudra775@gmail.com>
2025-10-01 00:04:13 +0530

    frontend

--- app/(app)/video-upload/page.tsx
+++ app/(app)/video-upload/page.tsx
@@ -2,7 +2,7 @@
 
 import React, {use, useState} from 'react'
 import axios from "axios"
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 function VideoUpload() {
   const [file, setFile] = useState<File | null>(null)
