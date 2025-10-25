918f1dfa7a2008ee34cc58dbcf8c0ba1ace0327b
Rudra775 <sharmarudra775@gmail.com>
2025-10-25 23:37:56 +0530

    refactor: used SSG for home page

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -3,6 +3,8 @@ import React, {useState, useEffect, useCallback} from 'react'
 import axios from 'axios'
 import VideoCard from '@/components/VideoCard'
 import { Video } from '@/types'
+export const dynamic = 'force-static';
+
 function Home() {
     const [videos, setVideos] = useState<Video[]>([])
     const [loading, setLoading] = useState(true)
