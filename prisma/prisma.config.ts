c6bd782de93aa87418f4942f99e4c87f75c1622d
Rudra775 <sharmarudra775@gmail.com>
2025-12-11 20:40:58 +0530

    polishing and refactoring the frameProcessor

---
+++ prisma/prisma.config.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import type { PrismaConfig } from "prisma";
+import { env } from "prisma/config";
+
+export default {
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: {
+    adapter: {
+      provider: "postgresql",
+      url: env("DATABASE_URL"),
+    }
+  }
+} satisfies PrismaConfig;
