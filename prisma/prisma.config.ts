ab99e2035fd8b24ac09200c878bcdf525372fdf0
Rudra775 <sharmarudra775@gmail.com>
2025-12-21 00:21:35 +0530

    update: migrate to latest version of prisma 7.2

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,14 +1,8 @@
-import 'dotenv/config'
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) throw new Error("Environment variable DATABASE_URL is not set");
+import { defineConfig, env } from "@prisma/config";
 
-export default {
+export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
-    provider: "postgresql",
-    url: databaseUrl,
-  }
-};
+    url: env("DATABASE_URL"),
+  },
+});
